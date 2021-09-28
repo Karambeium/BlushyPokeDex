5949dca272d36ef8a107a6a19bfc0400927e8198
Karam J <kjivani07@gmail.com>
2021-09-28 19:35:09 -0400

    Added async getPokemon Function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+import fetch from 'node-fetch'
 
+const dbURL = "http://https://pokeapi.co/api/v2/";
+
+let currentPokemon = {};
+
+/**
+ * 1. Get Trainers
+ * Implement a function that would retrieve all trainers fromt the db
+ * Endpoint: GET /trainer
+ */
+async function getPokemon() {
+    try {
+        let pokemon = await fetch(dbURL + "Bulbasaur");
+        let data = await pokemon.json();
+    }
+    catch {
+        console.log("Does Not Exist");
+    }
+}
