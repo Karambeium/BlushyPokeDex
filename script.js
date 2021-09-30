496085d2ad3a19fb23037a78adca11baa5712a8a
Karam J <kjivani07@gmail.com>
2021-09-30 19:55:13 -0400

    Stats and Moves Showing

--- script.js
+++ script.js
@@ -75,4 +75,27 @@ function checkIndex() {
 function populateData() {
     document.getElementById("pokemonImage").src = currentPokemonData.sprites.front_default;
     console.log(document.getElementById("pokemonImage").src);
+    let infoString = "";
+    infoString += 
+    document.getElementById("info").innerHTML = 
+        "Height: " + currentPokemonData.height + "<br>" + 
+        "Weight: " + currentPokemonData.weight + "<br>" +
+        "HP: " + currentPokemonData.stats[0].base_stat + "<br>" +
+        "Attack: " + currentPokemonData.stats[1].base_stat + "<br>" +
+        "Defense: " + currentPokemonData.stats[2].base_stat + "<br>" +
+        "Special Attack: " + currentPokemonData.stats[3].base_stat + "<br>" +
+        "Special Defense: " + currentPokemonData.stats[4].base_stat + "<br>" +
+        "Speed: " + currentPokemonData.stats[5].base_stat + "<br>";
+    document.getElementById("moves").innerHTML = "";
+    currentPokemonData.moves.forEach(move => document.getElementById("moves").innerHTML += (stringFormatter(move.move.name) + "<br>"));
+    console.log(document.getElementById("moves").innerHTML);
+}
+
+function stringFormatter(str) {
+    let temp = "";
+    let strArr = str.split("-");
+    strArr = strArr.map(string => string.substring(0,1).toUpperCase() + string.substring(1));
+    // strArr.forEach(string => string = string.toUpperCase());
+    strArr.forEach(string => temp += string + " ");
+    return(temp);
 }
